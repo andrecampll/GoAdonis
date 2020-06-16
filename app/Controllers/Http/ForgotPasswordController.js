68783d6911772104534f145546c8abaa7bc4e74e
André Victor <andrevictor50@gmail.com>
2020-06-16 09:03:18 -0300

    🚀 added email sending feature

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,7 @@
 'use strict'
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -12,7 +13,20 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.email)
+            .from('adonis@goadonis.com', 'Equipe GoAdonis')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (err) {
+      console.log(err)
+
       return response
         .status(err.status)
         .send({ error: { message: 'Algo não deu certo, esse email existe?' } })
